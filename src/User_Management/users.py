fad7dd874900bc16666b834bde3d2eddef8de4c0
MouhaBY <mohammed.beny@hotmail.fr>
2020-10-15 21:36:51 +0100

    items

---
+++ src/User_Management/users.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+
+class Users:
+    pass
+
+
+def main():
+    pass
+
+
+if __name__.endswith('__main__'):
+    main()
---
+++ src/items_exceptions.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+
+class ItemAlreadyStored(Exception):
+    pass
+
+
+class ItemNotStored(Exception):
+    pass
