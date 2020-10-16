0daa7269a38f97fc9bb7b7d0fad58922a2d96e82
MouhaBY <mohammed.beny@hotmail.fr>
2020-10-16 20:42:43 +0100

    adv

--- src/databaseInit.py
+++ src/databaseInit.py
@@ -9,16 +9,18 @@ import src.models.Users_table as Users_table
 import src.models.Pictures_table as Pictures_table
 
 def initialise_database():
+
     # Create database
     dbs.create_connection()
+
     # Create Tables
     Pictures_table.create()
     Companies_table.create()
     Contacts_table.create()
     Users_table.create()
+
     # Create default data
     Pictures_table.add(("filename", "0XDHFRE", "C:"))
-    print(Pictures_table.get("1"))
     Companies_table.add(("Company", "Comp-001", "Tunisia", "TVA01", "00216phone", "00216mobile", "www.company.com", "contact@company.com", 1))
     Contacts_table.add(("Mohammed", "BEN YAHIA", "Tunisia", "Matricule01", "00216phone", "00216mobile", "1", "Engineer", 1))
     Users_table.add(("MBY", "123", "mohammed.beny@hotmail.fr", "1", 1))
