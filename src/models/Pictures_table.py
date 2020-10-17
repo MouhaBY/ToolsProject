9137a63c3bbc459f52645b952943c9f7e7e9b21a
MouhaBY <mohammed.beny@hotmail.fr>
2020-10-17 15:45:42 +0100

    pictures table

--- src/models/Pictures_table.py
+++ src/models/Pictures_table.py
@@ -9,7 +9,7 @@ def create():
     sql = """ 
     CREATE TABLE "Pictures" (
     "id" INTEGER NOT NULL,
-    "filename" TEXT NOT NULL UNIQUE,
+    "filename" TEXT,
     "binary" TEXT,
     "filepath"	TEXT,
     PRIMARY KEY("id" AUTOINCREMENT)
@@ -36,15 +36,16 @@ def get(item):
         return None
 
 
-def check(item):
-    sql = "SELECT id FROM Pictures WHERE filename == (?)"
-    _result = dbs.execute_query(sql, (item,))
-    try:
-        return _result.fetchone()[0]
-    except TypeError:
-        return None
+# do not use filename not unique or null
+# def ckeck(item):
+#     sql = "SELECT id FROM Pictures WHERE filename == (?)"
+#     _result = dbs.execute_query(sql, (item,))
+#     try:
+#         return _result.fetchone()[0]
+#     except TypeError:
+#         return None
 
-
-def update(data):
-    sql = """ UPDATE Pictures SET filename= (?), binary= (?), filepath= (?) WHERE id == (?) """
-    dbs.execute_query(sql, data)
+# there is no delete in pictures
+# def update(data):
+#     sql = """ UPDATE Pictures SET filename= (?), binary= (?), filepath= (?) WHERE id == (?) """
+#     dbs.execute_query(sql, data)
